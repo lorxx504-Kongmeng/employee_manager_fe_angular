996b437ad0da1911ffcd71938f37fec8f74f46a1
kongmenglor <lorxx504@umn.edu>
2023-02-14 01:00:15 -0600

    init

---
+++ src/app/http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {IEmployee} from "./interfaces/IEmployee";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private apiSeverUrl = "";
+
+  constructor(private http:HttpClient) {}
+
+  public getEmployee(): Observable<IEmployee[]> {
+
+    return this.http.get<IEmployee[]>(`${this.apiSeverUrl}/employee/all`);
+  }
+}
---
+++ src/app/interfaces/IEmployee.ts
@@ -0,0 +1,10 @@
+export interface IEmployee {
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: number;
+  imageUrl: string;
+  code: string;
+  employeeId: number;
+}
